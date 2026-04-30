1629a2cc310a65ebd73659c165146d82acc280fb
ShivangChaurasia <shiva17ng@gmail.com>
2026-04-30 10:55:12 +0530

    Add multilingual support and file upload functionality
    - Updated web routes to support multiple languages (English, Hindi, Gujarati, Punjabi).
    - Created formController to handle displaying the form and processing file uploads.
    - Added language files for Gujarati, Hindi, and Punjabi with welcome messages, price, and duration.
    - Implemented a file upload form in the view.

---
+++ INT221/Project3/resources/views/form.blade.php
@@ -0,0 +1,5 @@
+<form method="post" action="submit" enctype="multipart/form-data">
+    @csrf
+    <input type="file">
+    <input type="submit">
+</form>
--- INT221/Project3/routes/web.php
+++ INT221/Project3/routes/web.php
@@ -3,5 +3,32 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    app()->setLocale(config('app.locale'));
     return view('welcome');
 });
+
+Route::get('en', function () {
+    app()->setLocale('en');
+    return view('welcome');
+});
+
+Route::get('hin', function () {
+    app()->setLocale('hin');
+    return view('welcome');
+});
+
+Route::get('guj', function () {
+    app()->setLocale('guj');
+    return view('welcome');
+});
+
+Route::get('pub', function () {
+    app()->setLocale('pub');
+    return view('welcome');
+});
+
+
+use App\Http\Controllers\formController;
+
+Route::get('show',[formController::class,"show"]);
+Route::get('submit',[formController::class,"submit"]);
